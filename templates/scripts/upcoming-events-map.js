8e69d29c63f4d1538af067f02df3347a62121f42
Ian <iansanders@mail.usf.edu>
2019-03-23 17:39:53 -0400

    Initial framework for home page

---
+++ templates/scripts/upcoming-events-map.js
@@ -0,0 +1,12 @@
+/**
+ * Initialize and setup upcoming events Google Maps widgets.
+ */
+
+const CONTAINER_CLASS = 'upcoming-events-map';
+
+/**
+ * Initialize all instances of `.upcoming-events-map` with Google Maps.
+ */
+function initMaps() {
+    console.log('Google Maps API loaded succesfully.');
+}
