cd227d841ed0fd33fb9d6100063de8988beccd40
Ian <iansanders@mail.usf.edu>
2019-03-23 18:25:11 -0400

    Complete working map and basic CSS

--- templates/scripts/upcoming-events-map.js
+++ templates/scripts/upcoming-events-map.js
@@ -8,5 +8,12 @@ const CONTAINER_CLASS = 'upcoming-events-map';
  * Initialize all instances of `.upcoming-events-map` with Google Maps.
  */
 function initMaps() {
-    console.log('Google Maps API loaded succesfully.');
+    const mapContainers = document.getElementsByClassName(CONTAINER_CLASS);
+
+    Array.from(mapContainers).forEach(container => {
+        new google.maps.Map(container, {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 5
+        });
+    });
 }
